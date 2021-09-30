da3b5e3d6f9e4d74f32b80228c33a4271a6e359a
Daniel Kurowski <daniel.kurowski@grifart.cz>
2021-09-30 15:09:25 +0200

    Implemented KeepUseStatemenetsDecorator

---
+++ src/Decorators/KeepUseStatementsDecorator.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types = 1);
+
+namespace Grifart\ClassScaffolder\Decorators;
+
+use Grifart\ClassScaffolder\ClassInNamespace;
+use Grifart\ClassScaffolder\Definition\ClassDefinition;
+
+
+final class KeepUseStatementsDecorator implements ClassDecorator
+{
+
+	public function decorate(ClassDefinition $definition, ClassInNamespace $draft, ?ClassInNamespace $current): void
+	{
+		if ($current === null) {
+			return;
+		}
+
+		foreach ($current->getNamespace()->getUses() as $use) {
+			$draft->getNamespace()->addUse($use);
+		}
+	}
+
+}
---
+++ tests/KeepUseStatementsDecorator/Stub/StubKeepUses.fromCurrent.phps
@@ -0,0 +1,9 @@
+namespace Grifart\ClassScaffolder\Test\KeepUseStatementsDecorator\Stub;
+
+use Grifart\ClassScaffolder\Definition\ClassDefinitionBuilder;
+use Grifart\ClassScaffolder\Definition\Field;
+
+final class StubKeepUsesFromCurrent
+{
+	private Field $field;
+}
