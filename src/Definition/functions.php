d7678ca7e878f5535d480f86f5f8b7e436dc6126
Jiří Pudil <me@jiripudil.cz>
2021-11-22 17:58:28 +0100

    add fields to define() function

--- src/Definition/functions.php
+++ src/Definition/functions.php
@@ -4,10 +4,16 @@ declare(strict_types = 1);
 
 namespace Grifart\ClassScaffolder\Definition;
 
-function define(ClassDefinition|string $definitionOrClassName): ClassDefinition {
-	if ($definitionOrClassName instanceof ClassDefinition) {
-		return $definitionOrClassName;
-	}
+/**
+ * @param array<string, Types\Type|ClassDefinition|string> $withFields
+ */
+function define(
+	ClassDefinition|string $definitionOrClassName,
+	array $withFields = [],
+): ClassDefinition {
+	$definition = $definitionOrClassName instanceof ClassDefinition
+		? $definitionOrClassName
+		: new ClassDefinition($definitionOrClassName);
 
-	return new ClassDefinition($definitionOrClassName);
+	return $definition->withFields($withFields);
 }
