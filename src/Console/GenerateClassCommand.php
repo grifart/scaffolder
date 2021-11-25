e0e6dbf664eb790f3760f4f7fd96d076f4e8d861
Jiří Pudil <me@jiripudil.cz>
2021-11-25 11:28:55 +0100

    Refactor command to keep results & restyle output

--- src/Console/GenerateClassCommand.php
+++ src/Console/GenerateClassCommand.php
@@ -12,34 +12,47 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 use Webmozart\PathUtil\Path;
 
 
 final class GenerateClassCommand extends Command
 {
+	private ClassGenerator $classGenerator;
+	private SymfonyStyle $style;
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->classGenerator = new ClassGenerator();
+	}
 
 	protected function configure(): void
 	{
 		$this->setName('grifart:scaffold')
 			->setDescription('Generate classes from given definitions.')
-			->addArgument('definition', InputArgument::OPTIONAL, 'Definition file or directory containing definitions', \getcwd())
-			->addOption('search-pattern', NULL, InputArgument::OPTIONAL, '(for directories) Search pattern for your definitions', '*.definition.php')
-			->addOption('dry-run', NULL, InputOption::VALUE_NONE, 'Only print the generated file to output instead of saving it')
-			->addOption('no-readonly', NULL, InputOption::VALUE_NONE, 'Generated files are marked as read only by default (using chmod), using this option turns off this behaviour.')
+			->addArgument('definition', InputArgument::OPTIONAL, 'Definition file or directory containing definitions.', \getcwd())
+			->addOption('search-pattern', NULL, InputArgument::OPTIONAL, 'Search pattern for your definition files.', '*.definition.php')
+			->addOption('no-readonly', NULL, InputOption::VALUE_NONE, 'Generated files are marked as read only by default (using chmod). Use this option to turn off this behaviour.')
+			->addOption('dry-run', NULL, InputOption::VALUE_NONE, 'Only print the generated file to output instead of saving it.')
 			->setAliases(['grifart:scaffolder:generateClass']);
 	}
 
-
 	protected function execute(InputInterface $input, OutputInterface $output): int
 	{
+		$this->style = new SymfonyStyle($input, $output);
+
 		$definitionPath = $input->getArgument('definition');
+		$cwd = \getcwd();
+		\assert(\is_string($definitionPath) && \is_string($cwd));
+		$definitionPath = Path::makeAbsolute($definitionPath, $cwd);
+
 		$searchPattern = $input->getOption('search-pattern');
-		$readonly = ! $input->getOption('no-readonly');
-		\assert(\is_string($definitionPath) && \is_string($searchPattern));
+		\assert(\is_string($searchPattern));
 
 		// 1. find files to process
 		$filesToProcess = [];
-		if(is_dir($definitionPath)) {
+		if (is_dir($definitionPath)) {
 			foreach(Finder::find($searchPattern)->from($definitionPath) as $definitionFile) {
 				$filesToProcess[] = (string) $definitionFile;
 			}
@@ -51,64 +64,95 @@ final class GenerateClassCommand extends Command
 		}
 
 		// 2. process them
-		$hasError = FALSE;
+
+		/** @var FileResult[] */
+		$results = [];
+		$isSuccess = true;
+
 		$processedFiles = 0;
 		$total = count($filesToProcess);
-		foreach($filesToProcess as $fileToProcess) {
-			$processedFiles++;
-			try {
-				$this->processFile($fileToProcess, $input, $output, $readonly);
-				$output->write("Processed $processedFiles / $total\r");
-			} catch (\Throwable $e) {
-				$hasError = TRUE;
-				$output->writeln(\sprintf("\n%s: <error>%s</error>", $fileToProcess, $e->getMessage()));
-				if (\class_exists(\Tracy\Debugger::class)) {
-					\Tracy\Debugger::log($e);
-				}
+
+		$output->writeln(\sprintf('Processing %d definition file%s:%s', $total, $total !== 1 ? 's' : '', \PHP_EOL));
+
+		foreach ($filesToProcess as $fileToProcess) {
+			$results[] = $result = $this->processFile($fileToProcess, $input);
+
+			if ($result->isSuccessful()) {
+				$output->write('.');
+			} else {
+				$output->write('<error>F</error>');
+				$isSuccess = false;
+			}
+
+			if (++$processedFiles % 40 === 0) {
+				$output->writeln('');
 			}
 		}
-		$output->writeln("<info>$processedFiles processed files.</info>");
-		if ($hasError) {
-			$output->writeln('<error>Some files wasn\'t processed correctly. See above.</error>');
-			return 1;
-		}
-		return 0;
+
+		$output->writeln(\PHP_EOL);
+
+		$this->printResults($results, $input, $output);
+
+		return (int) ! $isSuccess;
 	}
 
-	private function processFile(string $definitionFile, InputInterface $input, OutputInterface $output, bool $readonly): void {
-		foreach($this->loadDefinitions($definitionFile) as $definition) {
-			if (!$definition instanceof ClassDefinition) {
-				throw new \InvalidArgumentException('Definition file must contain class definition.');
-			}
-			$this->doGeneration($definition, $definitionFile, $input, $output, $readonly);
+	private function processFile(
+		string $definitionFile,
+		InputInterface $input,
+	): FileResult
+	{
+		try {
+			$definitions = $this->loadDefinitions($definitionFile);
+		} catch (\Throwable $error) {
+			return new FileResult($definitionFile, $error);
 		}
+
+		$result = new FileResult($definitionFile, null);
+		foreach ($definitions as $definition) {
+			$definitionResult = $this->generateClass($definition, $definitionFile, $input);
+			$result->addDefinition($definitionResult);
+		}
+
+		return $result;
 	}
 
-	private function doGeneration(ClassDefinition $definition, string $definitionFile, InputInterface $input, OutputInterface $output, bool $readonly): void {
-		$classGenerator = new ClassGenerator();
-		$generatedFile = $classGenerator->generateClass($definition);
+	private function generateClass(
+		ClassDefinition $definition,
+		string $definitionFile,
+		InputInterface $input,
+	): DefinitionResult
+	{
+		try {
+			$generatedFile = $this->classGenerator->generateClass($definition);
+		} catch (\Throwable $error) {
+			return DefinitionResult::error($definition, $error);
+		}
+
 		$code = (string) $generatedFile;
 
 		$targetPath = Path::join(
 			Path::getDirectory($definitionFile),
 			$definition->getClassName() . '.php'
 		);
+
 		if ($input->getOption('dry-run')) {
-			echo ' ---- ' . $targetPath . " ---- \n";
-			echo $code . "\n\n";
-
-		} else {
-			if (\file_exists($targetPath)) {
-				\chmod($targetPath, 0664); // some users accessing files using group permissions
-			}
-
-			\file_put_contents($targetPath, $code);
-			if ($readonly) {
-				\chmod($targetPath, 0444); // read-only -- assumes single user system
-			}
+			return DefinitionResult::success($definition, $code);
 		}
-	}
 
+		if (\file_exists($targetPath)) {
+			\chmod($targetPath, 0664); // some users accessing files using group permissions
+		}
+
+		if (\file_put_contents($targetPath, $code) === false) {
+			throw new \RuntimeException('Failed to write file.');
+		}
+
+		if ( ! $input->getOption('no-readonly')) {
+			\chmod($targetPath, 0444); // read-only -- assumes single user system
+		}
+
+		return DefinitionResult::success($definition, $code);
+	}
 
 	/**
 	 * @return ClassDefinition[]
@@ -118,25 +162,234 @@ final class GenerateClassCommand extends Command
 		$definitionFile = Path::canonicalize($definitionFile);
 		if ( ! \file_exists($definitionFile)) {
 			throw new \InvalidArgumentException(\sprintf(
-				'<error>Definition file not found at %s</error>',
+				'Definition file not found at %s',
 				$definitionFile
 			));
 		}
 
 		$definitions = require $definitionFile;
-		if ( ! \is_iterable($definitions)) {
+		if ($definitions instanceof ClassDefinition) {
 			$definitions = [$definitions];
 		}
-		foreach($definitions as $definition) {
+
+		if ( ! \is_iterable($definitions)) {
+			throw new \InvalidArgumentException(\sprintf(
+				'Definition file must return an iterable of ClassDefinition, %s received.',
+				\get_debug_type($definitions),
+			));
+		}
+
+		$count = 0;
+		foreach ($definitions as $definition) {
 			if ( ! ($definition instanceof ClassDefinition)) {
 				throw new \InvalidArgumentException(\sprintf(
-					'<error>Definition file must return instanceof ClassDefinition, %s received.</error>',
-					\is_object($definition) ? \get_class($definition) : \gettype($definition)
+					'Definition file must return instanceof ClassDefinition, %s received.',
+					\get_debug_type($definition),
 				));
 			}
+
+			$count++;
+		}
+
+		if ($count === 0) {
+			throw new \InvalidArgumentException('Definition file must return at least one ClassDefinition, empty list received.');
 		}
 
 		return $definitions;
 	}
 
+	/**
+	 * @param FileResult[] $results
+	 */
+	private function printResults(
+		array $results,
+		InputInterface $input,
+		OutputInterface $output,
+	): void
+	{
+		$isDryRun = (bool) $input->getOption('dry-run');
+		foreach ($results as $result) {
+			if ( ! $isDryRun && $result->isSuccessful() && ! $output->isVerbose()) {
+				continue;
+			}
+
+			$cwd = \getcwd();
+			\assert(\is_string($cwd));
+			$definitionFilePath = Path::makeRelative($result->getDefinitionFile(), $cwd);
+			$definitions = $result->getDefinitions();
+
+			$definitionFileError = $result->getError();
+			if ($definitionFileError !== null) {
+				$this->style->section(\sprintf(
+					'%s: <error>error loading definitions</error>',
+					$definitionFilePath,
+				));
+
+				$this->printError($definitionFileError, $output);
+
+				continue;
+			}
+
+			$errors = \array_filter($definitions, static fn(DefinitionResult $result) => ! $result->isSuccessful());
+			$this->style->section(\sprintf(
+				'%s: %d definition%s, %s',
+				$definitionFilePath,
+				\count($definitions),
+				\count($definitions) !== 1 ? 's' : '',
+				\count($errors) === 0 ? '<info>OK</info>' : \sprintf(
+					'<error>%d error%s</error>',
+					\count($errors),
+					\count($errors) > 1 ? 's' : '',
+				),
+			));
+
+			foreach ($definitions as $definition) {
+				if ( ! $isDryRun && $definition->isSuccessful() && ! $output->isVeryVerbose()) {
+					continue;
+				}
+
+				$output->writeln(\sprintf(
+					'%s %s%s',
+					$definition->isSuccessful() ? '<info> OK </info>' : '<error>FAIL</error>',
+					$definition->getClassName(),
+					\PHP_EOL,
+				));
+
+				if ( ! $definition->isSuccessful()) {
+					$error = $definition->getError();
+					\assert($error !== null);
+
+					$this->printError($error, $output);
+					$output->writeln('');
+
+				} elseif ($isDryRun) {
+					$code = $definition->getCode();
+					\assert($code !== null);
+
+					$output->writeln('');
+					$output->writeln($code . \PHP_EOL);
+				}
+			}
+		}
+	}
+
+	private function printError(\Throwable $error, OutputInterface $output): void
+	{
+		$this->style->error([
+			\sprintf(
+				'%s: %s',
+				\get_class($error),
+				$error->getMessage(),
+			),
+			\sprintf(
+				'in %s:%d',
+				$error->getFile(),
+				$error->getLine(),
+			),
+		]);
+
+		if ( ! \class_exists(\Tracy\Debugger::class)) {
+			return;
+		}
+
+		try {
+			$exceptionFile = \Tracy\Debugger::log($error);
+		} catch (\Throwable) {
+			return;
+		}
+
+		if (\is_string($exceptionFile)) {
+			$cwd = \getcwd();
+			\assert(\is_string($cwd));
+
+			$output->writeln(\sprintf(
+				'Error was logged in %s',
+				Path::makeRelative($exceptionFile, $cwd),
+			));
+		}
+	}
+}
+
+final class FileResult
+{
+	/** @var DefinitionResult[] */
+	private array $definitions = [];
+
+	public function __construct(
+		private string $definitionFile,
+		private \Throwable|null $error,
+	) {}
+
+	public function getDefinitionFile(): string
+	{
+		return $this->definitionFile;
+	}
+
+	public function getError(): \Throwable|null
+	{
+		return $this->error;
+	}
+
+	public function isSuccessful(): bool
+	{
+		foreach ($this->definitions as $result) {
+			if ( ! $result->isSuccessful()) {
+				return false;
+			}
+		}
+
+		return $this->error === null;
+	}
+
+	public function addDefinition(DefinitionResult $definitionResult): void
+	{
+		$this->definitions[] = $definitionResult;
+	}
+
+	/**
+	 * @return DefinitionResult[]
+	 */
+	public function getDefinitions(): array
+	{
+		return $this->definitions;
+	}
+}
+
+final class DefinitionResult
+{
+	private function __construct(
+		private ClassDefinition $definition,
+		private \Throwable|null $error,
+		private string|null $code,
+	) {}
+
+	public static function success(ClassDefinition $definition, string $code): self
+	{
+		return new self($definition, null, $code);
+	}
+
+	public static function error(ClassDefinition $definition, \Throwable $error): self
+	{
+		return new self($definition, $error, null);
+	}
+
+	public function getClassName(): string
+	{
+		return $this->definition->getFullyQualifiedName();
+	}
+
+	public function isSuccessful(): bool
+	{
+		return $this->error === null;
+	}
+
+	public function getError(): \Throwable|null
+	{
+		return $this->error;
+	}
+
+	public function getCode(): string|null
+	{
+		return $this->code;
+	}
 }
